e4e3539cc40031cbf413cc79aeb01eaebf13614a
whyucode <dronser8@gmail.com>
2022-06-30 19:52:51 +0530

    updated DB

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,6 @@ app.use(express.json());
 app.use(cors());
 app.use("/", routes);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port: ${5000}`);
 });
